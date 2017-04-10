4bf696b4297ad7df1b10e57f9c665a9be8b434e6
bgulbis <brian.gulbis@gmail.com>
2017-04-10 10:30:07 -0500

    run logistic regression

---
+++ src/03-logistic_regression.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(readxl)
+
+nm <- c("goal", "pressor_avg", "osmolality", "motility_agent")
+
+df <- read_excel("data/external/regression_data.xls", col_names = nm, skip = 1) %>%
+    dmap_at(c("goal", "motility_agent"), ~ .x == 1)
+
+mod <- glm(goal ~ pressor_avg + osmolality + motility_agent, df, family = "binomial")
